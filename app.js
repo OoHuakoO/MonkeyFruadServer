b4059e8b85c18eacb25e817dd2e52ab706fcbd8f
sokmok000 <sokmok000@gmail.com>
2021-04-02 16:41:34 +0700

    ccs

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
+const e = require("express");
 const express = require("express"),
   bodyParser = require("body-parser"),
-  // passport = require("passport"),
+  request = require("request"),
   flash = require("connect-flash"),
    cors = require('cors')
-  // passportLocal = require('passport-local'),
   userRoutes = require("./routes/User"),
   postRoutes = require("./routes/Post"),
   thiefRoutes = require("./routes/Thief"),
@@ -14,12 +14,16 @@ const express = require("express"),
  app.use(cors())
  
  app.post('/webhook', (req, res) => {
-  let tokenUser = req.body.events[0].replyToken
-  replyToUser(tokenUser)
-  res.sendStatus(200)
+   if(req.body.event){
+    let tokenUser = req.body.event[0].replyToken
+    console.log(tokenUser)
+    replyToUser(tokenUser)
+    res.sendStatus(200)
+   }
+  
 })
 
-const replyToUser = () => {
+const replyToUser = (tokenUser) => {
   let headers = {
     'Content-Type': 'application/json',
     'Authorization': 'Bearer OmVUWBXZY9gE9N/F6yHHXHIyoh1ahnKIpUOChGZmxqaUfjkucGYO8zRF2OGwPrYYyCxA3WmMQm3ToPt9A8IqgUB3WQT6nUeJTbSnGcDQ/wZRxvJqd5eAXHBQeBu4Mc8PJzQ1ZUHngoPvEFHIkoMlhgdB04t89/1O/w1cDnyilFU='
